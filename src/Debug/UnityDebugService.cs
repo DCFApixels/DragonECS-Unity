5f668bb5164d1af6b0f9ec8a6dd642defed2331f
Mikhail <99481254+DCFApixels@users.noreply.github.com>
2023-03-27 17:34:04 +0800

    Add debug feature

---
+++ src/Debug/UnityDebugService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Unity.Profiling;
+using UnityEngine;
+
+namespace DCFApixels.DragonECS.Unity
+{
+    public class UnityDebugService : DebugService
+    {
+        public static void Set()
+        {
+            DebugService.Set<UnityDebugService>();
+        }
+
+        private ProfilerMarker[] _profilerMarkers = new ProfilerMarker[64];
+
+        public override void Print(string tag, object v)
+        {
+            string log = $"[{tag}] {v}";
+            string taglower = tag.ToLower();
+            if (taglower.Contains("warning"))
+            {
+                Debug.LogWarning(log);
+                return;
+            }
+            if (taglower.Contains("error"))
+            {
+                Debug.LogError(log);
+                return;
+            }
+            Debug.Log(log);
+        }
+
+        public override void ProfileMarkBegin(int id)
+        {
+            _profilerMarkers[id].Begin();
+        }
+
+        public override double ProfileMarkEnd(int id)
+        {
+            _profilerMarkers[id].End();
+            return -1;
+        }
+
+        protected override void OnDelMark(int id)
+        {
+            _profilerMarkers[id] = default;
+        }
+
+        protected override void OnNewMark(int id, string name)
+        {
+            if (id >= _profilerMarkers.Length) Array.Resize(ref _profilerMarkers, _profilerMarkers.Length << 1);
+            _profilerMarkers[id] = new ProfilerMarker(ProfilerCategory.Scripts, name);
+        }
+
+    }
+}
