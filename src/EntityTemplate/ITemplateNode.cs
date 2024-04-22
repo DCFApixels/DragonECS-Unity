1a798dffbe5a0e2f2c111555b3c6c1e380633d7a
Mikhail <99481254+DCFApixels@users.noreply.github.com>
2024-04-22 16:34:31 +0800

    move ITemplateNode to Coore

--- src/EntityTemplate/ITemplateNode.cs
+++ src/EntityTemplate/ITemplateNode.cs
@@ -1,28 +1,12 @@
 ﻿namespace DCFApixels.DragonECS
 {
-    public interface ITemplateNode
-    {
-        void Apply(int worldID, int entityID);
-    }
     public interface ITemplate : ITemplateNode
     {
         //void Add(ITemplateNode template);
         //void Remove(ITemplateNode template);
     }
-    public static class ITemplateExtensions
+    public static class ITemplateNodeExtensions
     {
-        public static int NewEntity(this EcsWorld world, ITemplateNode template)
-        {
-            int e = world.NewEntity();
-            template.Apply(world.id, e);
-            return e;
-        }
-        public static entlong NewEntityLong(this EcsWorld world, ITemplateNode template)
-        {
-            entlong e = world.NewEntityLong();
-            template.Apply(world.id, e.ID);
-            return e;
-        }
         public static entlong NewEntityWithGameObject(this EcsWorld world, ITemplateNode template, string name = "Entity", GameObjectIcon icon = GameObjectIcon.NONE)
         {
             entlong e = world.NewEntityWithGameObject(name, icon);
