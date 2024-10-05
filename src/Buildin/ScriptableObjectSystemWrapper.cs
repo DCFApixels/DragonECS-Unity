7a4a1a9c4e473fe6a791f1de6899c92303b1840f
Mikhail <99481254+DCFApixels@users.noreply.github.com>
2024-10-05 18:27:06 +0800

    add ScriptableObjectSystemWrapper

---
+++ src/Buildin/ScriptableObjectSystemWrapper.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace DCFApixels.DragonECS
+{
+    [MetaName("ScriptableObjectSystem")]
+    [MetaColor(MetaColor.DragonCyan)]
+    [MetaDescription("Wrapper for ScriptableObject systems")]
+    public class ScriptableObjectSystemWrapper : IEcsModule
+    {
+        public ScriptableObject system;
+        public void Import(EcsPipeline.Builder b)
+        {
+            b.Add(system);
+        }
+    }
+}
