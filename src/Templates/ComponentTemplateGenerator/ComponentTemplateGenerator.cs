3d8b1551808dbca071bc8012d2938d060585b453
DCFApixels <99481254+DCFApixels@users.noreply.github.com>
2025-03-19 16:18:26 +0800

    Create ComponentTemplateGenerator.cs

---
+++ src/Templates/ComponentTemplateGenerator/ComponentTemplateGenerator.cs
@@ -0,0 +1,21 @@
+﻿using DCFApixels.DragonECS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCFApixels.Assets.Plugins.DragonECS_Unity.src.Templates.ComponentTemplateGenerator
+{
+    internal class ComponentTemplateGenerator
+    {
+    }
+
+    public abstract class Template_GUID<T> : ComponentTemplateBase<T> where T : struct, IEcsComponent
+    {
+        public override void Apply(short worldID, int entityID)
+        {
+            EcsPool<T>.Apply(ref component, entityID, worldID);
+        }
+    }
+}
