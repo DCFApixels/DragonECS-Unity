2642510c899da105bc6a917546708c6e297d8091
DCFApixels <99481254+DCFApixels@users.noreply.github.com>
2025-03-20 14:35:32 +0800

    Update UnityComponent.cs

--- src/Buildin/UnityComponent.cs
+++ src/Buildin/UnityComponent.cs
@@ -87,7 +87,7 @@ namespace DCFApixels.DragonECS.Unity.ComponentTemplates
     [MetaDescription(AUTHOR, "...")]
     [MovedFrom(true, "DCFApixels.DragonECS", "DCFApixels.DragonECS.Unity", "UnityComponentTransformTemplate")]
     [MetaID("DragonECS_843B8EF991013F1BFD9133437E1AFE9C")]
-    public sealed class Template_DragonECS_843B8EF991013F1BFD9133437E1AFE9C : UnityComponentTemplate<Transform> { }
+    public sealed class Template_DragonECS__843B8EF991013F1BFD9133437E1AFE9C : UnityComponentTemplate<Transform> { }
     [Serializable]
     [MetaTags(MetaTags.HIDDEN)]
     [MetaColor(MetaColor.DragonCyan)]
@@ -95,7 +95,7 @@ namespace DCFApixels.DragonECS.Unity.ComponentTemplates
     [MetaDescription(AUTHOR, "...")]
     [MovedFrom(true, "DCFApixels.DragonECS", "DCFApixels.DragonECS.Unity", "UnityComponentRigitBodyInitializer")]
     [MetaID("DragonECS_9A4B8EF99101396C44BF789C3215E9A9")]
-    public sealed class Template_DragonECS_9A4B8EF99101396C44BF789C3215E9A9 : UnityComponentTemplate<Rigidbody> { }
+    public sealed class Template_DragonECS__9A4B8EF99101396C44BF789C3215E9A9 : UnityComponentTemplate<Rigidbody> { }
     [Serializable]
     [MetaTags(MetaTags.HIDDEN)]
     [MetaColor(MetaColor.DragonCyan)]
@@ -103,7 +103,7 @@ namespace DCFApixels.DragonECS.Unity.ComponentTemplates
     [MetaDescription(AUTHOR, "...")]
     [MovedFrom(true, "DCFApixels.DragonECS", "DCFApixels.DragonECS.Unity", "UnityComponentAnimatorInitializer")]
     [MetaID("DragonECS_52598EF991016655335F234F20F44526")]
-    public sealed class Template_DragonECS_52598EF991016655335F234F20F44526 : UnityComponentTemplate<Animator> { }
+    public sealed class Template_DragonECS__52598EF991016655335F234F20F44526 : UnityComponentTemplate<Animator> { }
     [Serializable]
     [MetaTags(MetaTags.HIDDEN)]
     [MetaColor(MetaColor.DragonCyan)]
@@ -111,7 +111,7 @@ namespace DCFApixels.DragonECS.Unity.ComponentTemplates
     [MetaDescription(AUTHOR, "...")]
     [MovedFrom(true, "DCFApixels.DragonECS", "DCFApixels.DragonECS.Unity", "UnityComponentCharacterControllerInitializer")]
     [MetaID("DragonECS_AD658EF99101E8E38BB575D5353E7B1E")]
-    public sealed class Template_DragonECS_AD658EF99101E8E38BB575D5353E7B1E : UnityComponentTemplate<CharacterController> { }
+    public sealed class Template_DragonECS__AD658EF99101E8E38BB575D5353E7B1E : UnityComponentTemplate<CharacterController> { }
     #endregion
 
     #region Render Templates
@@ -122,7 +122,7 @@ namespace DCFApixels.DragonECS.Unity.ComponentTemplates
     [MetaDescription(AUTHOR, "...")]
     [MovedFrom(true, "DCFApixels.DragonECS", "DCFApixels.DragonECS.Unity", "UnityComponentMeshRendererTemplate")]
     [MetaID("DragonECS_6C6CA0F99101E80E013BCCCB5DA78FA5")]
-    public sealed class Template_DragonECS_6C6CA0F99101E80E013BCCCB5DA78FA5 : UnityComponentTemplate<MeshRenderer>
+    public sealed class Template_DragonECS__6C6CA0F99101E80E013BCCCB5DA78FA5 : UnityComponentTemplate<MeshRenderer>
     {
         public override MetaGroup Group { get { return UnityComponentConsts.RenderGroup; } }
     }
@@ -133,7 +133,7 @@ namespace DCFApixels.DragonECS.Unity.ComponentTemplates
     [MetaDescription(AUTHOR, "...")]
     [MovedFrom(true, "DCFApixels.DragonECS", "DCFApixels.DragonECS.Unity", "UnityComponentMeshFilterTemplate")]
     [MetaID("DragonECS_5475A1F9910109A138F609268B697A62")]
-    public sealed class Template_DragonECS_5475A1F9910109A138F609268B697A62 : UnityComponentTemplate<MeshFilter>
+    public sealed class Template_DragonECS__5475A1F9910109A138F609268B697A62 : UnityComponentTemplate<MeshFilter>
     {
         public override MetaGroup Group { get { return UnityComponentConsts.RenderGroup; } }
     }
@@ -144,7 +144,7 @@ namespace DCFApixels.DragonECS.Unity.ComponentTemplates
     [MetaDescription(AUTHOR, "...")]
     [MovedFrom(true, "DCFApixels.DragonECS", "DCFApixels.DragonECS.Unity", "UnityComponentSkinnedMeshRendererTemplate")]
     [MetaID("DragonECS_2C13A2F99101FAA3EA21BD351BF3B169")]
-    public sealed class Template_DragonECS_2C13A2F99101FAA3EA21BD351BF3B169 : UnityComponentTemplate<SkinnedMeshRenderer>
+    public sealed class Template_DragonECS__2C13A2F99101FAA3EA21BD351BF3B169 : UnityComponentTemplate<SkinnedMeshRenderer>
     {
         public override MetaGroup Group { get { return UnityComponentConsts.RenderGroup; } }
     }
@@ -155,7 +155,7 @@ namespace DCFApixels.DragonECS.Unity.ComponentTemplates
     [MetaDescription(AUTHOR, "...")]
     [MovedFrom(true, "DCFApixels.DragonECS", "DCFApixels.DragonECS.Unity", "UnityComponentSpriteRendererTemplate")]
     [MetaID("DragonECS_8B57A1F991016B2E1FC57D16F2D20A64")]
-    public sealed class Template_DragonECS_8B57A1F991016B2E1FC57D16F2D20A64 : UnityComponentTemplate<SpriteRenderer>
+    public sealed class Template_DragonECS__8B57A1F991016B2E1FC57D16F2D20A64 : UnityComponentTemplate<SpriteRenderer>
     {
         public override MetaGroup Group { get { return UnityComponentConsts.RenderGroup; } }
     }
@@ -169,7 +169,7 @@ namespace DCFApixels.DragonECS.Unity.ComponentTemplates
     [MetaDescription(AUTHOR, "...")]
     [MovedFrom(true, "DCFApixels.DragonECS", "DCFApixels.DragonECS.Unity", "UnityComponentColliderTemplate")]
     [MetaID("DragonECS_557F8EF9910132FE990CF50CBF368412")]
-    public sealed class Template_DragonECS_557F8EF9910132FE990CF50CBF368412 : UnityComponentTemplate<Collider>
+    public sealed class Template_DragonECS__557F8EF9910132FE990CF50CBF368412 : UnityComponentTemplate<Collider>
     {
         public override MetaGroup Group { get { return UnityComponentConsts.ColliderGroup; } }
     }
@@ -180,7 +180,7 @@ namespace DCFApixels.DragonECS.Unity.ComponentTemplates
     [MetaDescription(AUTHOR, "...")]
     [MovedFrom(true, "DCFApixels.DragonECS", "DCFApixels.DragonECS.Unity", "UnityComponentBoxColliderTemplate")]
     [MetaID("DragonECS_43669CF99101E94AB9EC19DC8EA3878B")]
-    public sealed class Template_DragonECS_43669CF99101E94AB9EC19DC8EA3878B : UnityComponentTemplate<BoxCollider>
+    public sealed class Template_DragonECS__43669CF99101E94AB9EC19DC8EA3878B : UnityComponentTemplate<BoxCollider>
     {
         public override MetaGroup Group { get { return UnityComponentConsts.ColliderGroup; } }
     }
@@ -191,7 +191,7 @@ namespace DCFApixels.DragonECS.Unity.ComponentTemplates
     [MetaDescription(AUTHOR, "...")]
     [MovedFrom(true, "DCFApixels.DragonECS", "DCFApixels.DragonECS.Unity", "UnityComponentSphereColliderTemplate")]
     [MetaID("DragonECS_749F9CF991017792E288D4E3B5BFE340")]
-    public sealed class Template_DragonECS_749F9CF991017792E288D4E3B5BFE340 : UnityComponentTemplate<SphereCollider>
+    public sealed class Template_DragonECS__749F9CF991017792E288D4E3B5BFE340 : UnityComponentTemplate<SphereCollider>
     {
         public override MetaGroup Group { get { return UnityComponentConsts.ColliderGroup; } }
     }
@@ -202,7 +202,7 @@ namespace DCFApixels.DragonECS.Unity.ComponentTemplates
     [MetaDescription(AUTHOR, "...")]
     [MovedFrom(true, "DCFApixels.DragonECS", "DCFApixels.DragonECS.Unity", "UnityComponentCapsuleColliderTemplate")]
     [MetaID("DragonECS_72B09CF99101A33EBC4410B0FD8375E0")]
-    public sealed class Template_DragonECS_72B09CF99101A33EBC4410B0FD8375E0 : UnityComponentTemplate<CapsuleCollider>
+    public sealed class Template_DragonECS__72B09CF99101A33EBC4410B0FD8375E0 : UnityComponentTemplate<CapsuleCollider>
     {
         public override MetaGroup Group { get { return UnityComponentConsts.ColliderGroup; } }
     }
@@ -213,7 +213,7 @@ namespace DCFApixels.DragonECS.Unity.ComponentTemplates
     [MetaDescription(AUTHOR, "...")]
     [MovedFrom(true, "DCFApixels.DragonECS", "DCFApixels.DragonECS.Unity", "UnityComponentMeshColliderTemplate")]
     [MetaID("DragonECS_3BBC9CF99101F7C00989D2E55A40EF1B")]
-    public sealed class Template_DragonECS_3BBC9CF99101F7C00989D2E55A40EF1B : UnityComponentTemplate<MeshCollider>
+    public sealed class Template_DragonECS__3BBC9CF99101F7C00989D2E55A40EF1B : UnityComponentTemplate<MeshCollider>
     {
         public override MetaGroup Group { get { return UnityComponentConsts.ColliderGroup; } }
     }
@@ -227,7 +227,7 @@ namespace DCFApixels.DragonECS.Unity.ComponentTemplates
     [MetaDescription(AUTHOR, "...")]
     [MovedFrom(true, "DCFApixels.DragonECS", "DCFApixels.DragonECS.Unity", "UnityComponentJointTemplate")]
     [MetaID("DragonECS_1AC79CF99101C4279852BB6AE12DC61B")]
-    public sealed class Template_DragonECS_1AC79CF99101C4279852BB6AE12DC61B : UnityComponentTemplate<Joint>
+    public sealed class Template_DragonECS__1AC79CF99101C4279852BB6AE12DC61B : UnityComponentTemplate<Joint>
     {
         public override MetaGroup Group { get { return UnityComponentConsts.JointGroup; } }
     }
@@ -238,7 +238,7 @@ namespace DCFApixels.DragonECS.Unity.ComponentTemplates
     [MetaDescription(AUTHOR, "...")]
     [MovedFrom(true, "DCFApixels.DragonECS", "DCFApixels.DragonECS.Unity", "UnityComponentFixedJointTemplate")]
     [MetaID("DragonECS_E3D99CF991016428C6688672052C6F4E")]
-    public sealed class Template_DragonECS_E3D99CF991016428C6688672052C6F4E : UnityComponentTemplate<FixedJoint>
+    public sealed class Template_DragonECS__E3D99CF991016428C6688672052C6F4E : UnityComponentTemplate<FixedJoint>
     {
         public override MetaGroup Group { get { return UnityComponentConsts.JointGroup; } }
     }
@@ -249,7 +249,7 @@ namespace DCFApixels.DragonECS.Unity.ComponentTemplates
     [MetaDescription(AUTHOR, "...")]
     [MovedFrom(true, "DCFApixels.DragonECS", "DCFApixels.DragonECS.Unity", "UnityComponentCharacterJointTemplate")]
     [MetaID("DragonECS_7BE59CF99101322AE307229E1466B225")]
-    public sealed class Template_DragonECS_7BE59CF99101322AE307229E1466B225 : UnityComponentTemplate<CharacterJoint>
+    public sealed class Template_DragonECS__7BE59CF99101322AE307229E1466B225 : UnityComponentTemplate<CharacterJoint>
     {
         public override MetaGroup Group { get { return UnityComponentConsts.JointGroup; } }
     }
@@ -260,7 +260,7 @@ namespace DCFApixels.DragonECS.Unity.ComponentTemplates
     [MetaDescription(AUTHOR, "...")]
     [MovedFrom(true, "DCFApixels.DragonECS", "DCFApixels.DragonECS.Unity", "UnityComponentConfigurableJointTemplate")]
     [MetaID("DragonECS_FBF29CF99101EE07543CFF460854B1F6")]
-    public sealed class Template_DragonECS_FBF29CF99101EE07543CFF460854B1F6 : UnityComponentTemplate<ConfigurableJoint>
+    public sealed class Template_DragonECS__FBF29CF99101EE07543CFF460854B1F6 : UnityComponentTemplate<ConfigurableJoint>
     {
         public override MetaGroup Group { get { return UnityComponentConsts.JointGroup; } }
     }
